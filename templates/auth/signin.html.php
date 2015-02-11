a9ff33624bb3a6b5238adfd80d204c0fde31cef8
Naresh Jain <nashjain@gmail.com>
2015-02-12 01:48:05 +0530

    cleaned up the layout for the sign-in form

--- templates/auth/signin.html.php
+++ templates/auth/signin.html.php
@@ -1,84 +1,53 @@
 <section class="wrapper style special fade">
     <div class="container">
-        <div class="row">
+        <div class="row uniform 50%">
             <div class="6u 12u$(medium)">
-                <form action="/auth/login" method="post" class="form-signin login-form" align="left">
-                    <h2 class="form-signin-heading" style="margin-bottom: 15px;">Login</h2>
+                <form action="/auth/login" method="post">
+                    <h2>Login</h2>
                     <?php
                         $requested_url = '/';
                         if(array_key_exists('requested_url', $data)) $requested_url =$data['requested_url'];
                         echo "<input type='hidden' name='requested_url' value='$requested_url'>";
                     ?>
-                    <div class="control-group">
-                        <label for="email">Email<sup>*</sup></label>
+                    <div class="row uniform 50%">
+                        <div class="3u 12u$(medium)"><label for="email" class="form-label">Email<sup>*</sup></label></div>
+                        <div class="7u 12u$(medium)"><input type="email" name="email" id="email" placeholder="Email Address" required></div>
+                        <div class="2u$ 12u$(medium)"></div>
 
-                        <div class="controls">
-                            <input type="email" name="email" id="email" placeholder="Email Address" required>
-                        </div>
+                        <div class="3u 12u$(medium)"><label for="password" class="form-label">Password<sup>*</sup></label></div>
+                        <div class="7u 12u$(medium)"><input type="password" name="password" id="password" placeholder="Password" required minlength="6"></div>
+                        <div class="2u$ 12u$(medium)"></div>
+
+                        <div class="4u 12u$(medium)"></div>
+                        <div class="4u 12u$(medium)"><input type="submit" value="Login" class="fit special" /></div>
+                        <div class="4u$ 12u$(medium)"></div>
+
+                        <div class="4u 12u$(medium)"></div>
+                        <div class="4u 12u$(medium)" style="text-align: center"><a href="/auth/forgot-password" class="fit special" style="color: #ffffff">Forgot Password?</a></div>
+                        <div class="4u$ 12u$(medium)"></div>
                     </div>
-                    <br>
-
-                    <div class="control-group">
-                        <label for="password">Password<sup>*</sup></label>
-
-                        <div class="controls">
-                            <input type="password" name="password" id="password" placeholder="Password" required minlength="6">
-                        </div>
-                    </div>
-                    <br>
-
-                    <div class="control-group">
-                        <div class="controls">
-                            <input type="submit" value="Login" class="fit special" />
-                        </div>
-                    </div>
-                    <hr>
-                    <div style="text-align: center"><a href="/auth/forgot-password" class="fit special" style="color: #ffffff">Forgot Password?</a></div>
                 </form>
             </div>
-            <div class="6u 12u$(medium)">
-                <form id="theForm" action="/auth/registration" method="post" class="form-signin registration-form" align="left">
-                    <h2 class="form-signin-heading" style="margin-bottom: 15px;">Register</h2>
-                    <div class="control-group">
-                        <label for="email">Name<sup>*</sup></label>
+            <div class="6u$ 12u$(medium)">
+                <form id="theForm" action="/auth/registration" method="post">
+                    <h2>Register</h2>
+                    <div class="row uniform 50%">
+                        <div class="4u 12u$(medium)"><label for="name" class="form-label">Name<sup>*</sup></label></div>
+                        <div class="8u$ 12u$(medium)"><input type="text" name="name" id="name" placeholder="Name" required minlength="3"></div>
 
-                        <div class="controls">
-                            <input name="name" type="text" placeholder="Name" required minlength="3" autocomplete="off">
-                        </div>
-                    </div>
-                    <br>
+                        <div class="4u 12u$(medium)"><label for="new_email" class="form-label">Email<sup>*</sup></label></div>
+                        <div class="8u$ 12u$(medium)"><input type="email" name="email" id="new_email" placeholder="Email Address" required></div>
 
-                    <div class="control-group">
-                        <label for="new_email">Email<sup>*</sup></label>
+                        <div class="4u 12u$(medium)"><label for="new_password" class="form-label">Password<sup>*</sup></label></div>
+                        <div class="8u$ 12u$(medium)"><input type="password" name="password" id="new_password" placeholder="Password" required minlength="6" autocomplete="off"></div>
 
-                        <div class="controls">
-                            <input type="email" name="email" id="new_email" placeholder="Email Address" autocomplete="off" required>
-                        </div>
-                    </div>
-                    <br>
+                        <div class="4u 12u$(medium)"><label for="confirmation-password" class="form-label">Confirm Password<sup>*</sup></label></div>
+                        <div class="8u$ 12u$(medium)"><input type="password" name="confirmation-password" id="confirmation-password" placeholder="Confirm Password" required minlength="6"></div>
 
-                    <div class="control-group">
-                        <label for="new_password">Password<sup>*</sup></label>
+                        <div class="4u 12u$(medium)"></div>
+                        <div class="4u 12u$(medium)"><input type="submit" value="Create Account" class="fit special" /></div>
+                        <div class="4u$ 12u$(medium)"></div>
 
-                        <div class="controls">
-                            <input type="password" name="password" id="new_password" placeholder="Password" required minlength="6" autocomplete="off">
-                        </div>
-                    </div>
-                    <br>
-
-                    <div class="control-group">
-                        <label for="confirmation-password">Confirm Password<sup>*</sup></label>
-
-                        <div class="controls">
-                            <input type="password" name="confirmation-password" id="confirmation-password" placeholder="Confirm Password" required minlength="6">
-                        </div>
-                    </div>
-                    <br>
-
-                    <div class="control-group">
-                        <div class="controls">
-                            <input type="submit" value="Create Account" class="fit special" />
-                        </div>
                     </div>
                 </form>
                 <?php include_once TEMPLATE_PATH. "inc/jquery_validator.php"; ?>
