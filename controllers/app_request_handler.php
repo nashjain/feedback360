8cb224bc8679590b6adda36c0fd39649d3a2645d
Naresh Jain <nashjain@gmail.com>
2015-02-11 14:55:33 +0530

    got rid of the sample model class

--- controllers/app_request_handler.php
+++ controllers/app_request_handler.php
@@ -3,8 +3,6 @@
 use phpish\app;
 use phpish\template;
 
-include_once MODELS_DIR . "model.php";
-
 app\get("/", function($req) {
     $data = [];
     return template\compose("home.html", compact('data'), "layout-no-sidebar.html");
